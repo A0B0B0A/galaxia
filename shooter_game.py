1ab187515048ce1c75497b8634713d4eda410bad
A0B0B0A <152081588+A0B0B0A@users.noreply.github.com>
2024-06-16 17:23:39 +0300

    Add files via upload

--- shooter_game.py
+++ shooter_game.py
@@ -1,5 +1,6 @@
 from pygame import *
 import random
+import sys
 
 init()  
 mixer.init()
@@ -18,7 +19,7 @@ game_over_text = font1.render("Game Over!", True, (255, 0, 0))
 
 screen_info = display.Info()
 
-WIDTH, HEIGHT = screen_info.current_w, screen_info.current_h,
+WIDTH, HEIGHT = screen_info.current_w, screen_info.current_h
 window = display.set_mode((WIDTH, HEIGHT), flags = FULLSCREEN)
 FPS = 90
 clock = time.Clock()
@@ -32,6 +33,7 @@ bg_y2 = -HEIGHT
 p_img = image.load('images-removebg-preview.png')
 warrior_img = image.load('h-removebg-preview.png')
 bullet_img = image.load('JSDF-removebg-preview.png')
+
 all_sprites = sprite.Group()
 
 class NPC(sprite.Sprite):
@@ -69,7 +71,7 @@ class Player(NPC):
                 self.bg_speed += 0.1
         if key_pressed[K_s] and self.rect.bottom < HEIGHT:
             self.rect.y += self.speed
-            if self.bg_speed > 0:
+            if self.bg_speed > 2:
                 self.bg_speed -= 0.3
         if key_pressed[K_d] and self.rect.right < WIDTH:
             self.rect.x += self.speed
@@ -123,49 +125,78 @@ spawn_interval = random.randint(500, 3000)
 
 finish = False
 run = True
+
+start_screen = True
+start_text = font2.render(f"PRESS ANY KEY TO START", True, (255, 255, 255))
+finish_text = font2.render(f"PRESS F TO RESTART", True, (255, 255, 255))
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
+            sys.exit()
         elif e.type == KEYDOWN:
+            if start_screen:
+                start_screen = False
             if e.key == K_ESCAPE:
                 run = False
-            if e.key == K_SPACE:
+                sys.exit()
+            if e.key == K_SPACE and not finish and not start_screen:
                 player.fire()
+            if finish:
+                if e.key == K_f:
+                    finish= False
+                    for s in all_sprites:
+                        s.kill()
+                        player = Player(p_img, 100, 80, 300, 300)
+                        score_aboba_text = font2.render(f"Score: {player.score}", True, (255, 255, 255))
 
-    window.blit(bg,(0, bg_y1))
-    window.blit(bg,(0, bg_y2))
-    bg_y1 += player.bg_speed
-    bg_y2 += player.bg_speed
-    
+
     if bg_y1 > HEIGHT:
         bg_y1 = -HEIGHT
     if bg_y2 > HEIGHT:
         bg_y2 = -HEIGHT
-    if player.hp <= 0 :
-        finish = True
-    
-    all_sprites.draw(window)
 
-    if not finish:
-        all_sprites.update()
+    window.blit(bg,(0, bg_y1))
+    window.blit(bg,(0, bg_y2))
     
-    bullets_collide = sprite.groupcollide(bullets, boses, True, True, sprite.collide_mask)
-    for bullet in bullets_collide:
-        player.score += 10
-        score_aboba_text = font2.render(f"Score: {player.score}", True, (255, 255, 255))
-    now = time.get_ticks()
+    if start_screen:
+        window.blit(start_text, (WIDTH/2 - start_text.get_width()/2, HEIGHT/2 - start_text.get_height()/2))
+    else:
+        bg_y1 += player.bg_speed
+        bg_y2 += player.bg_speed
+        
 
-    if now - alien_spawn_time > spawn_interval:
-        rand_n = random.randint(1, 3)
-        for _ in range(rand_n):
-            alien1 = Bos(warrior_img, 80, 60)
-        alien_spawn_time = time.get_ticks()
-        spawn_interval = random.randint(500, 3000)
-    if finish:
-        window.blit(game_over_text, (WIDTH/2 - game_over_text.get_width()/2, HEIGHT/2 - game_over_text.get_height()/2))
-    
-    window.blit(score_aboba_text, (0, 0))
+        if player.hp <= 0 :
+            finish = True
+        
+        all_sprites.draw(window)
+
+        if not finish:
+            all_sprites.update()
+        
+        bullets_collide = sprite.groupcollide(bullets, boses, True, True, sprite.collide_mask)
+        for bullet in bullets_collide:
+            player.score += 10
+            score_aboba_text = font2.render(f"Score: {player.score}", True, (255, 255, 255))
+        now = time.get_ticks()
+
+        if now - alien_spawn_time > spawn_interval:
+            rand_n = random.randint(1, 3)
+            for _ in range(rand_n):
+                alien1 = Bos(warrior_img, 80, 60)
+            alien_spawn_time = time.get_ticks()
+            spawn_interval = random.randint(500, 3000)
+        if finish:
+            window.blit(game_over_text, (WIDTH/2 - game_over_text.get_width()/2,
+                                          HEIGHT/2 - game_over_text.get_height()/2))
+
+            window.blit(finish_text, (WIDTH/2 - finish_text.get_width()/2,
+                                       HEIGHT/2 - finish_text.get_height()/2 + 100))
+
+        window.blit(score_aboba_text, (0, 0))
 
     display.update()
     clock.tick(FPS)
+#cntl + shift + p
+#env, vnv 
+#download pygame and modyls
